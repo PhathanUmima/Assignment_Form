93d9283e1d4280c9ed874712942031bfcc7af749
PhathanUmima <91871514+PhathanUmima@users.noreply.github.com>
2023-10-11 15:04:31 +0530

    Update RomanToInt.java

--- RomanToInt.java
+++ RomanToInt.java
@@ -5,7 +5,7 @@ public class Main {
    public static void main(String args[]) {
       Main obj = new Main();
       Scanner sc = new Scanner(System.in);
-      System.out.print("Enter a Roman Number in capital letters: ");
+      System.out.print("Enter a Roman Number in capital letters of I and X: ");
       String inputRoman= sc.nextLine();
       System.out.println("The Integer value of given Roman number is:"+obj.romanToInt(inputRoman));
    }
